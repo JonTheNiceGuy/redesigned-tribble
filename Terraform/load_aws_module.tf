6af24c0552bf0ece6d14ddded69c3821bae0a99f
Jon Spriggs <jon.spriggs@uk.fujitsu.com>
2020-03-26 13:19:29 +0000

    Fixed typo in AWX ip

--- Terraform/load_aws_module.tf
+++ Terraform/load_aws_module.tf
@@ -1,10 +1,10 @@
 module "AWS" {
   source                    = "./AWS"
-  modulename                = "AWS"
+  modulename                = "cust1"
   public_first_three_octets = "198.18.3"
   myip                      = "${trimspace(data.http.icanhazip.body)}/32"
   key                       = data.local_file.key_file.content
-  awx_public_ip             = module.Mgmt1.awxip
+  awx_public_ip             = module.Mgmt_AWX.awxip
   vm_user                   = "fgtadmin"
   vm_password               = trimspace(data.local_file.admin_password.content)
 
--- Terraform/load_azure_module.tf
+++ Terraform/load_azure_module.tf
@@ -1,10 +1,10 @@
 module "Azure" {
   source                    = "./Azure"
-  modulename                = "Azure"
+  modulename                = "cust2"
   public_first_three_octets = "198.18.5"
   myip                      = "${trimspace(data.http.icanhazip.body)}/32"
   key                       = data.local_file.key_file.content
-  awx_public_ip             = module.Mgmt1.awxip
+  awx_public_ip             = module.Mgmt_AWX.awxip
   vm_user                   = "fgtadmin"
   vm_password               = trimspace(data.local_file.admin_password.content)
   
